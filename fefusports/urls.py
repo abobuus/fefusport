e32da42febcbe73d68a0bc01a8adca6c3a634d0a
zenci125 <121859719+zenci125@users.noreply.github.com>
2023-11-19 19:43:00 +1000

    add signup

--- fefusports/urls.py
+++ fefusports/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users.views import signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path('signup/', signup, name='signup'),
 ]
