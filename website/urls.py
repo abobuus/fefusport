77ea4b62c146721e5c52bdbe98870551615640fb
Vladislav Sukhodolov <LUCIFER190504@mail.ru>
2023-11-21 05:37:04 +1000

    create apppage def

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('addpage/', addpage, name='add_page'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from website.forms import AddPostForm
+from website.models import *
 
 
 def index(request):
-    return render(request, 'website/index.html', {'title': 'Главная страница'})
+    tags = Tag.objects.all()
+    questionnaires = Person.objects.all()
+    return render(request, 'website/index.html', {'tags': tags, 'questionnaires': questionnaires})
+
+
+def addpage(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+    return render(request, 'website/addpage.html', {'form': form, 'title': 'Добавление анкеты'})
